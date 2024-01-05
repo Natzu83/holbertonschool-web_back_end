efdc29e869e500c80de194d3be60ecbb5f90d676
Natzu83 <natzuac@yahoo.com>
2024-01-05 10:21:06 -0400

    Appropriate types

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,1 +1,20 @@
-Placeholder
+#!/usr/bin/env python3
+"""
+The protype function 'element_length',
+was provided and we must annotate it.
+"""
+from typing import Iterable, Sequence, List, Tuple
+
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """
+    Returns a list of tuples that each contain:
+    the corresponding item in 'lst', and its len (int).
+
+    'lst' is assumed to be an iterable (you should be able
+    to iterate over it with a for loop),
+    and each item in 'lst' is assumed to have
+    the '__len__' attribute (you should be able
+    to call 'len(<item>)'.
+    """
+    return [(i, len(i)) for i in lst]
