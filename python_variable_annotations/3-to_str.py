8cbf67f79636a28c14d5a8cfea877666be121c6a
Natzu83 <natzuac@yahoo.com>
2024-01-05 09:45:30 -0400

    Adding the comments

--- python_variable_annotations/3-to_str.py
+++ python_variable_annotations/3-to_str.py
@@ -1,3 +1,14 @@
 #!/usr/bin/env python3
+"""
+A function named 'to_str' , uses the Python type hints to specify that
+'to_str' takes in a 'float' and returns a 'str'.
+"""
+
+
 def to_str(n: float) -> str:
+    """
+    Assuming that 'n' is a float,
+    returns the string representation
+    of 'n'.
+    """
     return str(n)
