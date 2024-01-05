8e3a62706697f7768c5ac9a6c8e99e07961f058e
Natzu83 <natzuac@yahoo.com>
2024-01-05 09:07:42 -0400

    First try

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,1 +1,18 @@
-Placeholder
+#!/usr/bin/env python3
+"""
+Function 'add', which
+takes 2 arguments, and returns their sum.
+
+Uses Python type hints to specify that 'add'
+takes float arguments and returns a float.
+"""
+
+
+def add(a: float, b: float) -> float:
+    """
+    Returns a + b.
+
+    'a', 'b' and the returned result
+    are assumed to be of type 'float'.
+    """
+    return a + b
