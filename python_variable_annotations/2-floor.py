5b895c06f8288ae31ba0b789fb97e80c57b743e3
Natzu83 <natzuac@yahoo.com>
2024-01-05 09:25:59 -0400

    First try

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,1 +1,8 @@
-Placeholder
+def floor(n: float) -> int:
+    """
+    Assuming that 'n' is a float,
+    returns the floored version of 'n',
+    which should be an 'int', since 'n'
+    is assumed to be a 'float'.
+    """
+    return int(n)
