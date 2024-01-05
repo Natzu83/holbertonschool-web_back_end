6237243731defc388ada1bbbe7ccef1ad2563975
Natzu83 <natzuac@yahoo.com>
2024-01-05 10:38:22 -0400

    Types of annotations

--- python_variable_annotations/101-safely_get_value.py
+++ python_variable_annotations/101-safely_get_value.py
@@ -11,4 +11,13 @@ def safely_get_value(
             dct: Mapping,
             key: Any,
             default: Union[T, type(None)] = None) -> Union[Any, T]:
-    
+    """
+    If 'key' is a valid 'key' in 'dct',
+        this function returns 'dct[key]'.
+    Otherwise,
+        this function returns 'default'.
+    """
+    if key in dct:
+        return dct[key]
+    else:
+        return default
