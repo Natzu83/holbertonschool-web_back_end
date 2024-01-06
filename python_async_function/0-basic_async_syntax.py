bf7cf3e8fe07a841b73abb768f5af2636980bbb5
Natzu83 <natzuac@yahoo.com>
2024-01-05 20:11:30 -0400

    First try

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,1 +1,26 @@
-Placeholder
+#!/usr/bin/env python3
+"""
+'Hello World!' figue in Python's
+documentation for 'asyncio'.
+
+Check it out!
+https://docs.python.org/3/library/asyncio.html
+"""
+import asyncio
+from random import uniform
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """
+    Chooses a random number
+    (somewhat) in the range of
+    0 and 'max_delay',
+    using 'random.uniform'.
+
+    awaits for that amount of time IN SECONDS.
+
+    returns the number chosen.
+    """
+    wait_time: float = uniform(0, max_delay)
+    await asyncio.sleep(wait_time)
+    return wait_time
