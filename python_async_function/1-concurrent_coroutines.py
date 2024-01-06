6b11f67a79e0f66abf17d8ad96a924401bc8c832
Natzu83 <natzuac@yahoo.com>
2024-01-05 20:20:24 -0400

    Back to old version

--- python_async_function/1-concurrent_coroutines.py
+++ python_async_function/1-concurrent_coroutines.py
@@ -1,21 +1,10 @@
 #!/usr/bin/env python3
 """
-Creates a coroutine that runs 'wait_random(max_delay)'
+Routine that runs 'wait_random(max_delay)'
 n times, all in parallel, and returns a list
 of the results of each 'wait_random' call
 (which should be the amount of time the coroutine took
 ot run)
-
-The result list of times should be in ascending order.
-To do this, this file has an INTERMEDIATE COROUTINE
-that ACTUALLY RUNS and awaits for 'wait_random(max_delay)',
-and appends that call's result to a result list parameter.
-
-'wait_n' just runs 'run_and_append(max_delay, <result list>)'
-n times with 'asyncio.gather', and each 'run_and_append'
-coroutine appends its result when it's ready, to achieve
-appending the results of the 'wait_random' calls
-in the order that they finish.
 """
 import asyncio
 from typing import List
@@ -39,16 +28,6 @@ async def wait_n(n: int, max_delay: int) -> List[seconds]:
     and returns a list of the amount of time
     (in floats representing seconds) each 'wait_random'
     call took, IN THE ORDER THAT THEY FINISHED.
-
-    This function achieves this, by running an intermediary
-    coroutine, 'run_and_append(max_delay, result)',
-    that awaits 'wait_random(max_delay)', then
-    appends its result to 'result', which should be the
-    result list.
-
-    This allows the amount of time each 'wait_random'
-    call took to be appended to 'result' IN THE
-    ORDER THEY FINISHED, AND TO BE RUN IN PARALLEL.
 
     Returns a list of floats that represent
     the time, IN SECONDS,
