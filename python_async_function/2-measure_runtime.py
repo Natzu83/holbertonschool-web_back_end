b6efd69e08278e1b6bb96127fd33f2e222110c94
Natzu83 <natzuac@yahoo.com>
2024-01-06 08:22:59 -0400

    Make it float

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -1,1 +1,42 @@
-Placeholder
+#!/usr/bin/env python3
+"""
+Imports 'wait_n' 1-concurrent_coroutines.py,
+'asyncio' and 'time' from 'time'.
+
+Makes a function that calls
+runs 'wait_n(n, max_delay)',
+and using 'time',
+measures the amount of time
+'wait_n(...)' took to run,
+then returns it divided by n.
+
+The time is returned as a float,
+representing milliseconds.
+"""
+import asyncio
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+milliseconds = float
+
+
+def measure_time(n: int, max_delay: int) -> milliseconds:
+    """
+    Runs 'wait_n(n, max_delay)' using asyncio.
+
+    Then, this function
+    returns the amount of time that took to run,
+    divided by 'n'.
+
+    The amount of time is measured by using
+    'time.process_time'.
+
+    The type of this function's returned result
+    is a float representing milliseconds.
+    """
+    before: milliseconds = time.process_time()
+    asyncio.run(wait_n(n, max_delay))
+    after: milliseconds = time.process_time()
+
+    total_time: milliseconds = after - before
+    return total_time / n
