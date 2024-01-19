0c92f01a5781e56afd1a3186eab112bff2830e45
Natzu83 <natzuac@yahoo.com>
2024-01-19 19:02:43 -0400

    Add the disable

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -1,1 +1,14 @@
-Placeholder
+/* eslint-disable */
+export default function taskBlock(trueOrFalse) {
+    const task = false;
+    const task2 = true;
+  
+    if (trueOrFalse) {
+      /* eslint-disable */
+      const task = true;
+      const task2 = false;
+      /* eslint-enable */
+    }
+  
+    return [task, task2];
+  }
