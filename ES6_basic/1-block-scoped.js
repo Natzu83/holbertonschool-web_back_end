169511b9f178c7aa013607bd2b4597415e094a29
Natzu83 <natzuac@yahoo.com>
2024-01-19 19:05:11 -0400

    Hot fix

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -2,12 +2,10 @@
 export default function taskBlock(trueOrFalse) {
     const task = false;
     const task2 = true;
-  
+    /* eslint-disable no-unused-vars */
     if (trueOrFalse) {
-      /* eslint-disable */
       const task = true;
       const task2 = false;
-      /* eslint-enable */
     }
   
     return [task, task2];
