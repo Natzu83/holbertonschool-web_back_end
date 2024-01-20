4db62ff9f98553b7f9752ef3d5adc06686eafb8a
Natzu83 <natzuac@yahoo.com>
2024-01-19 20:01:14 -0400

    Get budget

--- ES6_basic/7-getBudgetObject.js
+++ ES6_basic/7-getBudgetObject.js
@@ -1,1 +1,10 @@
-Placeholder
+/* eslint-disable */
+export default function getBudgetObject(income, gdp, capita) {
+    const budget = {
+      income,
+      gdp,
+      capita,
+    };
+  
+    return budget;
+  }
