24f9d9480e1c46d8bbdef82c1157fbb2cb8799e2
Natzu83 <natzuac@yahoo.com>
2024-01-26 22:08:10 -0400

    Spaces

--- ES6_promise/2-then.js
+++ ES6_promise/2-then.js
@@ -2,10 +2,10 @@ export default function handleResponseFromAPI(promise) {
   return promise
     .then(() => {
       console.log('Got a response from the API');
-  return { status: 200, body: 'success' };
+      return { status: 200, body: 'success' };
     })
-      .catch(() => {
-        console.error('');
-        return new Error();
-      });
+    .catch(() => {
+      console.error('');
+      return new Error();
+    });
 }
