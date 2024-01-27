e621f781cbf885e02f6ae1bdc34e9ca512e0b621
Natzu83 <natzuac@yahoo.com>
2024-01-26 21:56:02 -0400

    Please work

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,7 +1,5 @@
 export default function getResponseFromAPI() {
-    function settlePromise(resolve, reject) { // eslint-disable-line no-unused-vars
-      resolve('did nothing');
-    }
-  
-    return new Promise(settlePromise);
+    return new Promise((resolve) => {
+      resolve();
+    });
   }
