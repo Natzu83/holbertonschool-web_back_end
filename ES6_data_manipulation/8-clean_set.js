520d9b296c832a302ff8876a713ee0c919d6ee65
Natzu83 <natzuac@yahoo.com>
2024-02-02 19:19:32 -0400

    Spaces

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,12 +1,12 @@
 export default function cleanSet(set, startString) {
   if (startString === '' || typeof startString !== 'string' || startString.length === 0) {
     return '';
-      }
-      const str = [];
-      set.forEach((element) => {
-  if (element && element.startsWith(startString)) {
-    str.push(element.slice(startString.length));
-      }
+  }
+  const str = [];
+  set.forEach((element) => {
+    if (element && element.startsWith(startString)) {
+      str.push(element.slice(startString.length));
+    }
   });
   return str.join('-');
 }
