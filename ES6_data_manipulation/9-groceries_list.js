2ceaaf2b34c785450963bdb619a5b55447fda4c3
Natzu83 <natzuac@yahoo.com>
2024-02-02 19:24:06 -0400

    Groceries

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -1,1 +1,11 @@
-Placeholder
+export default function updateUniqueItems(map) {
+  if (!(map instanceof Map)) {
+    throw new Error('Cannot process');
+  }
+    for (const [key, value] of map) {
+    if (value === 1) {
+      map.set(key, 100);
+  }
+}
+  return map;
+}
