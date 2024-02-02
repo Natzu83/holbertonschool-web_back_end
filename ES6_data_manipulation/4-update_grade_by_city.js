b4b51985669031aa9b68a819b59c191a06d1d103
Natzu83 <natzuac@yahoo.com>
2024-02-02 17:30:11 -0400

    Upgrade

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,1 +1,9 @@
-Placeholder
+export default function updateStudentGradeByCity(students, city, newGrades) {
+  return students
+    .filter((student) => student.location === city)
+    .map((student) => {
+      const gradeObj = newGrades.find((grade) => grade.studentId === student.id);
+      const grade = gradeObj ? gradeObj.grade : 'N/A';
+    return { ...student, grade };
+  });
+}
