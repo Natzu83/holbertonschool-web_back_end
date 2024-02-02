282fcdb3347e32459a371583475de5d3700b1d44
Natzu83 <natzuac@yahoo.com>
2024-02-02 18:21:07 -0400

    Clean set

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,1 +1,12 @@
-Placeholder
+export default function cleanSet(set, startString) {
+  if (startString === '' || typeof startString !== 'string' || startString.length === 0) {
+    return '';
+      }
+      const str = [];
+      set.forEach((element) => {
+  if (element && element.startsWith(startString)) {
+    str.push(element.slice(startString.length));
+      }
+  });
+  return str.join('-');
+}
